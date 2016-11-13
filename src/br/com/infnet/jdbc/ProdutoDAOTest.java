089685cc5f52acb6a22bda5ce85099360ee75dee
edveloso <edveloso@gmail.com>
2016-11-13 16:08:29 -0200

    Tirando o reflection para priorizar commons lang.

--- src/br/com/infnet/jdbc/ProdutoDAOTest.java
+++ src/br/com/infnet/jdbc/ProdutoDAOTest.java
@@ -12,17 +12,8 @@ public class ProdutoDAOTest {
 	@Test
 	public void testConexao() throws Exception {
 		ProdutoDAO dao = new ProdutoDAO();
-		Class<? extends ProdutoDAO> daoClass = dao.getClass();
-		Field conField = daoClass.getDeclaredField("conexao");
-		conField.setAccessible(true);
-		assertNotNull(conField.get(dao));
-		
 		Field conF = FieldUtils.getDeclaredField(ProdutoDAO.class, "conexao", true);
 		assertNotNull( conF.get(dao) );
-		
-		
-		
-		
 	}
 
 }
